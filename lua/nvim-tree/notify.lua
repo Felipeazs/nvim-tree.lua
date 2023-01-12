d0e82e0a97146abf5aa8465aa3ece77a928de55f
felipeazs <felipeazs@gmail.com>
2023-01-12 14:15:20 -0300

    update

--- lua/nvim-tree/notify.lua
+++ lua/nvim-tree/notify.lua
@@ -1,45 +1,45 @@
 local M = {}
 
 local config = {
-  threshold = vim.log.levels.INFO,
+    threshold = vim.log.levels.INFO,
 }
 
 local modes = {
-  { name = "trace", level = vim.log.levels.TRACE },
-  { name = "debug", level = vim.log.levels.DEBUG },
-  { name = "info", level = vim.log.levels.INFO },
-  { name = "warn", level = vim.log.levels.WARN },
-  { name = "error", level = vim.log.levels.ERROR },
+    { name = "trace", level = vim.log.levels.TRACE },
+    { name = "debug", level = vim.log.levels.DEBUG },
+    { name = "info", level = vim.log.levels.INFO },
+    { name = "warn", level = vim.log.levels.WARN },
+    { name = "error", level = vim.log.levels.ERROR },
 }
 
 do
-  local has_notify, notify_plugin = pcall(require, "notify")
+    local has_notify, notify_plugin = pcall(require, "notify")
 
-  local dispatch = function(level, msg)
-    if level < config.threshold then
-      return
+    local dispatch = function(level, msg)
+        if level < config.threshold then
+            return
+        end
+
+        vim.schedule(function()
+            if has_notify and notify_plugin then
+                --notify_plugin(msg, level, { title = "NvimTree" })
+                print(msg)
+            else
+                vim.notify(string.format("[NvimTree] %s", vim.inspect(msg)), level)
+            end
+        end)
     end
 
-    vim.schedule(function()
-      if has_notify and notify_plugin then
-        --notify_plugin(msg, level, { title = "NvimTree" })
-        print(msg)
-      else
-        vim.notify(string.format("[NvimTree] %s", vim.inspect(msg)), level)
-      end
-    end)
-  end
-
-  for _, x in ipairs(modes) do
-    M[x.name] = function(msg)
-      return dispatch(x.level, msg)
+    for _, x in ipairs(modes) do
+        M[x.name] = function(msg)
+            return dispatch(x.level, msg)
+        end
     end
-  end
 end
 
 function M.setup(opts)
-  opts = opts or {}
-  config.threshold = opts.notify.threshold or vim.log.levels.INFO
+    opts = opts or {}
+    config.threshold = opts.notify.threshold or vim.log.levels.INFO
 end
 
 return M
