3dc441d12cdc76ea980751a9b4b04fde64053d5d
Felipe A. Zapata Soto <72353999+Felipeazs@users.noreply.github.com>
2023-01-12 11:56:29 -0300

    Update notify.lua

--- lua/nvim-tree/notify.lua
+++ lua/nvim-tree/notify.lua
@@ -22,7 +22,7 @@ do
 
     vim.schedule(function()
       if has_notify and notify_plugin then
-          print(msg)
+        vim.notify(msg)
         notify_plugin(msg, level, { title = "NvimTree" })
       else
         vim.notify(string.format("[NvimTree] %t", vim.inspect(msg)), level)
